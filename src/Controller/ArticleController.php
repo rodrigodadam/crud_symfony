1252cd8996883f3b4ef1f99d37ca8c5b11dc0e73
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-19 20:09:07 +0000

    ArticleController edit with template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,11 +6,17 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController
+class ArticleController extends Controller
 {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index()
     {
-        return new Response('<html><body>Hello World</body></html>');
+//        return new Response('<html><body>Hello World</body></html>');
+        return $this->render('articles/index.html.twig');
     }
 }
