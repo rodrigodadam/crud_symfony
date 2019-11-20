da03e233448882455754f5dbec4ff61d8ace8f2f
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-20 09:48:29 +0000

    New functions include in ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,9 +6,16 @@ namespace App\Controller;
 use App\Entity\Article;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use function Sodium\add;
 
 class ArticleController extends Controller
 {
@@ -23,6 +30,31 @@ class ArticleController extends Controller
     }
 
     /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+                ))
+                ->add('save', SubmitType::class, array(
+                    'label' => 'Create',
+                    'attr' => array('class' => 'btn btn-primary mt-3')
+                ))
+                ->getForm();
+
+            return $this->render('articles/new.html.twig', array(
+                'form' => $form->createView()
+            ));
+    }
+
+    /**
      * @Route("/article/{id}", name="article_show")
      */
     public function show($id)
@@ -31,8 +63,6 @@ class ArticleController extends Controller
 
         return $this->render('articles/show.html.twig', array('article' => $article));
     }
-
-
 
 //    /**
 //     * @Route("/article/save")
