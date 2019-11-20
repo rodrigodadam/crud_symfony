519ddd5b4ebfce87808478418de5f84c1fa9d283
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-20 16:49:13 +0000

    EditController implementation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -31,44 +31,7 @@ class ArticleController extends Controller
 
 
 
-    /**
-     * @Route("/article/edit/{id}",
-     *     name="edit_article",
-     *     methods={"GET", "POST"})
-     */
-    public function edit(Request $request, $id)
-    {
 
-        $article = new Article();
-        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-
-        $form = $this->createFormBuilder($article)
-            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
-            ->add('body', TextareaType::class, array(
-                'required' => false,
-                'attr' => array('class' => 'form-control')
-            ))
-            ->add('save', SubmitType::class, array(
-                'label' => 'Create',
-                'attr' => array('class' => 'btn btn-primary mt-3')
-            ))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-//            $article = $form->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->flush();
-
-            return $this->redirectToRoute('article_list');
-        }
-
-        return $this->render('articles/edit.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
 
     /**
      * @Route("/article/{id}",
--- src/Controller/users/EditController.php
+++ src/Controller/users/EditController.php
@@ -3,19 +3,52 @@
 
 namespace App\Controller\users;
 
+use App\Controller\ArticleController;
 use App\Entity\Article;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class EditController
+class EditController extends ArticleController
 {
+    /**
+     * @Route("/article/edit/{id}",
+     *     name="edit_article",
+     *     methods={"GET", "POST"})
+     */
+    public function edit(Request $request, $id)
+    {
 
+        $article = new Article();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
