1d374a2121a5ad0afd4f490ca6132cf3cfe11f4c
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-20 16:46:45 +0000

    CreateController implementation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,43 +29,7 @@ class ArticleController extends Controller
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
-    /**
-     * @Route("/article/new",
-     *     name="new_article",
-     *     methods={"GET", "POST"})
-     */
-    public function new(Request $request)
-    {
-        $article = new Article();
 
-        $form = $this->createFormBuilder($article)
-            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
-            ->add('body', TextareaType::class, array(
-                'required' => false,
-                'attr' => array('class' => 'form-control')
-            ))
-            ->add('save', SubmitType::class, array(
-                'label' => 'Create',
-                'attr' => array('class' => 'btn btn-primary mt-3')
-            ))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $article = $form->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($article);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('article_list');
-        }
-
-        return $this->render('articles/new.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
 
     /**
      * @Route("/article/edit/{id}",
--- src/Controller/users/CreateController.php
+++ src/Controller/users/CreateController.php
@@ -3,19 +3,52 @@
 
 namespace App\Controller\users;
 
+use App\Controller\ArticleController;
 use App\Entity\Article;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class CreateController extends Controller
+class CreateController extends ArticleController
 {
+    /**
+     * @Route("/article/new",
+     *     name="new_article",
+     *     methods={"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
 
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
