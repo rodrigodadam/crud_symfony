8f822d74109bff6ba173592d4f063deb8fc87b49
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-20 08:58:13 +0000

    article cass

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -19,5 +21,23 @@ class ArticleController extends Controller
 //        return new Response('<html><body>Hello World</body></html>');
         $articles = ['Article 1', 'Article 2'];
         return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Article One');
+        $article->setBody('This is the body for article one');
+
+        $entityManager->persist($article);
+
+        $entityManager->flush();
+
+        return new Response('Save an article with the id of' . $article->getId());
     }
 }
